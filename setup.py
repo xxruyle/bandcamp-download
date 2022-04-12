9f920c57ce8f4fc880b251bc642507bca8f5d37c
Theodicy1 <xrcnerd@gmail.com>
2022-04-11 21:14:20 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+#work in progress 
+setup(
+    name='Theodicy1',
+    version='0.0.1',
+    author = 'Xavier Ruyle',
+    author_email='xxruyle@gmail.com',
+    packages=find_packages()
+)
