448a3cba4f87754ab453ea2caef218da4a15a55b
Theodicy1 <xrcnerd@gmail.com>
2022-04-11 20:52:08 -0500

    refactoring

--- bandcampdownload/__init__.py
+++ bandcampdownload/__init__.py
@@ -2,5 +2,3 @@ from bandcampdownload.objects import linkfinder
 from bandcampdownload.objects import downloader
 from bandcampdownload.objects import meta_info
 from bandcampdownload.pathdir import PATH
-from dotenv import load_dotenv
-import os 
